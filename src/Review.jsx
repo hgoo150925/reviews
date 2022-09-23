44b496554bbb680dddeefc4fe51963256763139a
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-23 20:41:47 -0300

    agregando funcionalidad

--- src/Review.jsx
+++ src/Review.jsx
@@ -8,6 +8,30 @@ export const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = number => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+
+    return number;
+  };
+
+  const prevPerson = () => {
+    setIndex(index => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const nextPerson = () => {
+    setIndex(index => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -21,10 +45,10 @@ export const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button onClick={prevPerson} className="prev-btn">
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
+        <button onClick={nextPerson} className="next-btn">
           <FaChevronRight />
         </button>
       </div>
