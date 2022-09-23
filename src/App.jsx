20d679407f7129087cd70de5c02d475de4800077
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-23 20:05:27 -0300

    creando estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import Review from './Review';
+import { BsGithub } from 'react-icons/bs';
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
